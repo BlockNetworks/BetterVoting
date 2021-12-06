9c4ff57223a73c1bc081d2134ca20800ad6e06c8
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2021-12-05 17:35:26 -0800

    Update BetterVoting.php

--- src/twisted/bettervoting/BetterVoting.php
+++ src/twisted/bettervoting/BetterVoting.php
@@ -21,7 +21,7 @@ use function defined;
 use function explode;
 use function strtoupper;
 
-final class BetterVoting extends PluginBase{
+final class BetterVoting extends PluginBase {
 
 	/** @var PiggyCustomEnchants|null */
 	private $piggyCustomEnchants;
@@ -29,7 +29,7 @@ final class BetterVoting extends PluginBase{
 	/** @var BetterVotingThread */
 	private $thread;
 
-	public function onEnable() : void{
+	public function onEnable() : void {
 		$this->piggyCustomEnchants = $this->getServer()->getPluginManager()->getPlugin("PiggyCustomEnchants");
 
 		$this->thread = $thread = new BetterVotingThread();
@@ -39,12 +39,12 @@ final class BetterVoting extends PluginBase{
 		$this->getServer()->getCommandMap()->register("bettervoting", new VoteCommand($this));
 		$this->getServer()->getPluginManager()->registerEvents(new BetterVotingListener($this), $this);
 
-		$this->getScheduler()->scheduleRepeatingTask(new ClosureTask(static function() use ($thread) : void{
+		$this->getScheduler()->scheduleRepeatingTask(new ClosureTask(static function() use ($thread) : void {
 			$thread->addActionToQueue(BetterVotingThread::ACTION_UPDATE_CACHE);
 
 		}), BetterVotingCache::TIME_BETWEEN_CACHE_UPDATE * 20);
 		$server = $this->getServer();
-		$this->getScheduler()->scheduleRepeatingTask(new ClosureTask(static function() use ($thread, $server) : void{
+		$this->getScheduler()->scheduleRepeatingTask(new ClosureTask(static function() use ($thread, $server) : void {
 			$thread->collectActionResults($server);
 		}), 1);
 	}
@@ -54,50 +54,50 @@ final class BetterVoting extends PluginBase{
 	 *
 	 * @return BetterVotingThread
 	 */
-	public function getVoteThread() : BetterVotingThread{
+	public function getVoteThread() : BetterVotingThread {
 		return $this->thread;
 	}
 
 	/**
 	 * Reload the plugin's config and all of it's data.
 	 */
-	public function loadConfig() : void{
+	public function loadConfig() : void {
 		$this->reloadConfig();
 		$config = $this->getConfig();
 
 		$this->thread->setApiKey((string) $config->get("api-key", ""));
 
 		$commands = [];
-		foreach($config->get("commands", []) as $command){
+		foreach ($config->get("commands", []) as $command) {
 			$commands[] = TextFormat::colorize($command);
 		}
 		BetterVotingCache::setCommands($commands);
 
 		$items = [];
-		foreach($config->get("items") as $itemInfo){
+		foreach ($config->get("items") as $itemInfo) {
 			$parts = explode(":", $itemInfo);
 
 			$item = ItemFactory::fromString($parts[0] . ":" . ($parts[1] ?? 0));
 			$item->setCount((int) ($parts[2] ?? 1) > 0 ? (int) ($parts[2] ?? 1) : 1);
-			if(($parts[3] ?? "default") !== "default"){
+			if (($parts[3] ?? "default") !== "default") {
 				$item->setCustomName(TextFormat::colorize($parts[3]));
 			}
 
-			if(count($parts) > 4){
+			if (count($parts) > 4) {
 				$enchants = array_slice($parts, 4);
-				for($i = 0; $i < count($enchants); $i += 2){
+				for ($i = 0; $i < count($enchants); $i += 2) {
 					$enchant = null;
-					if($this->piggyCustomEnchants !== null){
+					if ($this->piggyCustomEnchants !== null) {
 						$const = CustomEnchantIds::class . "::" . strtoupper($enchants[$i] ?? "");
-						if(defined($const)){
+						if (defined($const)) {
 							$enchant = CustomEnchant::getEnchantment(constant($const));
 						}
 					}
-					if($enchant === null){
+					if ($enchant === null) {
 						$enchant = Enchantment::getEnchantmentByName($enchants[$i] ?? "");
 					}
 
-					if($enchant !== null){
+					if ($enchant !== null) {
 						$level = (int) ($enchants[$i + 1] ?? 1);
 						$level = $level > 0 ? $level : 1;
 
@@ -109,4 +109,5 @@ final class BetterVoting extends PluginBase{
 		}
 		BetterVotingCache::setItems($items);
 	}
+
 }
