2fdc6d6c328d78dc7900cdaee6a2232f45342d9e
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2021-12-05 17:34:15 -0800

    Update BetterVotingThread.php

--- src/twisted/bettervoting/thread/BetterVotingThread.php
+++ src/twisted/bettervoting/thread/BetterVotingThread.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace twisted\bettervoting\thread;
@@ -31,7 +32,7 @@ use const CURLOPT_RETURNTRANSFER;
 use const CURLOPT_URL;
 use const JSON_ERROR_NONE;
 
-final class BetterVotingThread extends Thread{
+final class BetterVotingThread extends Thread {
 
 	/**
 	 * Identifiers used to identify actions between threads.
@@ -58,47 +59,39 @@ final class BetterVotingThread extends Thread{
 	/** @var bool */
 	private $running = false;
 
-	public function __construct(){
+	public function __construct() {
 		$this->actionQueue = new Threaded();
 		$this->actionResults = new Threaded();
 	}
 
-	public function run() : void{
+	public function run() : void {
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_FORBID_REUSE, true);
 		curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
-		while($this->running){
-			while(($action = $this->actionQueue->shift()) !== null){
+		while ($this->running) {
+			while (($action = $this->actionQueue->shift()) !== null) {
 				$action = igbinary_unserialize($action);
-				switch($action["type"]){
+				switch ($action["type"]) {
 					case self::ACTION_VALIDATE_VOTE:
 						$player = $action["player"];
 						curl_setopt($ch, CURLOPT_URL, $this->getApiUrl("object=votes&element=claim&key=" . $this->apiKey . "&username=" . str_replace(" ", "%20", $player)));
 						curl_setopt($ch, CURLOPT_POST, false);
-
 						$action["result"] = curl_exec($ch);
-
 						$this->actionResults[] = igbinary_serialize($action);
-
 						break;
 					case self::ACTION_CLAIM_VOTE:
 						$player = $action["player"];
 						curl_setopt($ch, CURLOPT_URL, $this->getApiUrl("action=post&object=votes&element=claim&key=" . $this->apiKey . "&username=" . str_replace(" ", "%20", $player)));
 						curl_setopt($ch, CURLOPT_POST, true);
-
 						$action["result"] = curl_exec($ch);
-
 						$this->actionResults[] = igbinary_serialize($action);
-
 						break;
 					case self::ACTION_UPDATE_CACHE:
 						curl_setopt($ch, CURLOPT_URL, $this->getApiUrl("object=servers&element=detail&key=" . $this->apiKey));
 						curl_setopt($ch, CURLOPT_POST, false);
-
 						$infoResult = curl_exec($ch);
 						$action["info"] = [
 							"uptime" => "0%",
@@ -108,9 +101,9 @@ final class BetterVotingThread extends Thread{
 							"favorited" => "0",
 							"comments" => "0"
 						];
-						if($infoResult !== false){
+						if ($infoResult !== false) {
 							$info = json_decode($infoResult, true);
-							if(json_last_error() === JSON_ERROR_NONE){
+							if (json_last_error() === JSON_ERROR_NONE) {
 								$action["info"] = [
 									"uptime" => $info["uptime"] . "%",
 									"score" => $info["score"],
@@ -121,16 +114,14 @@ final class BetterVotingThread extends Thread{
 								];
 							}
 						}
-
 						curl_setopt($ch, CURLOPT_URL, $this->getApiUrl("object=servers&element=voters&key=" . $this->apiKey . "&month=current&format=json&limit=10"));
 						curl_setopt($ch, CURLOPT_POST, false);
-
 						$topResult = curl_exec($ch);
 						$action["top"] = [];
-						if($topResult !== false){
+						if ($topResult !== false) {
 							$top = json_decode($topResult, true);
-							if(json_last_error() === JSON_ERROR_NONE){
-								foreach($top["voters"] as $voter){
+							if (json_last_error() === JSON_ERROR_NONE) {
+								foreach ($top["voters"] as $voter) {
 									$action["top"][$voter["nickname"]] = $voter["votes"];
 								}
 							}
@@ -141,44 +132,39 @@ final class BetterVotingThread extends Thread{
 
 						$unclaimedResult = curl_exec($ch);
 						$action["unclaimed"] = [];
-						if($unclaimedResult !== false){
+						if ($unclaimedResult !== false) {
 							$votes = json_decode($unclaimedResult, true);
-							if(json_last_error() === JSON_ERROR_NONE){
-								foreach($votes["votes"] as $vote){
+							if (json_last_error() === JSON_ERROR_NONE) {
+								foreach ($votes["votes"] as $vote) {
 									$date = new DateTime("now", new DateTimeZone("America/New_York"));
-									if(strpos($vote["date"], $date->format("F jS, Y")) === 0 && $vote["claimed"] === "0"){
+									if (strpos($vote["date"], $date->format("F jS, Y")) === 0 && $vote["claimed"] === "0") {
 										$action["unclaimed"][] = $vote["nickname"];
 									}
 								}
 							}
 						}
-
 						$this->actionResults[] = igbinary_serialize($action);
-
 						break;
 				}
 			}
-
 			$this->sleep();
 		}
-
 		curl_close($ch);
 	}
 
-	public function sleep() : void{
-		$this->synchronized(function() : void{
-			if($this->running){
+	public function sleep() : void {
+		$this->synchronized(function() : void {
+			if ($this->running) {
 				$this->wait();
 			}
 		});
 	}
 
-	public function quit() : void{
+	public function quit() : void {
 		$this->running = false;
-		$this->synchronized(function() : void{
+		$this->synchronized(function() : void {
 			$this->notify();
 		});
-
 		parent::quit();
 	}
 
@@ -189,7 +175,7 @@ final class BetterVotingThread extends Thread{
 	 *
 	 * @return string
 	 */
-	private function getApiUrl(string $args) : string{
+	private function getApiUrl(string $args) : string {
 		return "https://minecraftpocket-servers.com/api/?" . $args;
 	}
 
@@ -199,14 +185,13 @@ final class BetterVotingThread extends Thread{
 	 * @param int         $action
 	 * @param Player|null $player
 	 */
-	public function addActionToQueue(int $action, ?Player $player = null) : void{
+	public function addActionToQueue(int $action, ?Player $player = null) : void {
 		$toAdd = ["type" => $action];
-		if($player !== null){
+		if ($player !== null) {
 			$toAdd["player"] = $player->getName();
 		}
 		$this->actionQueue[] = igbinary_serialize($toAdd);
-
-		$this->synchronized(function() : void{
+		$this->synchronized(function() : void {
 			$this->notify();
 		});
 	}
@@ -219,14 +204,13 @@ final class BetterVotingThread extends Thread{
 	 *
 	 * @return bool
 	 */
-	public function isActionInQueue(int $action, ?Player $player = null) : bool{
-		foreach($this->actionQueue as $queued){
+	public function isActionInQueue(int $action, ?Player $player = null) : bool {
+		foreach ($this->actionQueue as $queued) {
 			$queued = igbinary_unserialize($queued);
-			if($queued["type"] === $action && ($player === null || $queued["player"] === $player->getName())){
+			if ($queued["type"] === $action && ($player === null || $queued["player"] === $player->getName())) {
 				return true;
 			}
 		}
-
 		return false;
 	}
 
@@ -235,46 +219,40 @@ final class BetterVotingThread extends Thread{
 	 *
 	 * @param Server $server
 	 */
-	public function collectActionResults(Server $server) : void{
+	public function collectActionResults(Server $server) : void {
 		$plugin = $server->getPluginManager()->getPlugin("BetterVoting");
-		if($plugin instanceof BetterVoting && $plugin->isEnabled()){
-			while(($result = $this->actionResults->shift()) !== null){
+		if ($plugin instanceof BetterVoting && $plugin->isEnabled()) {
+			while (($result = $this->actionResults->shift()) !== null) {
 				$result = igbinary_unserialize($result);
-				switch($result["type"]){
+				switch ($result["type"]) {
 					case self::ACTION_VALIDATE_VOTE:
 						$player = $server->getPlayerExact($result["player"]);
-						if($player !== null && $player->isOnline()){
-							switch($result["result"]){
+						if ($player !== null && $player->isOnline()) {
+							switch ($result["result"]) {
 								case self::VOTE_STATUS_NOT_VOTED:
 									$player->sendMessage(TextFormat::colorize($plugin->getConfig()->getNested("messages.vote.notvoted", "&cYou have not voted yet.")));
-
 									break;
 								case self::VOTE_STATUS_CLAIMABLE:
 									($event = new PlayerVoteEvent($player))->call();
-									if($event->isCancelled()){
+									if ($event->isCancelled()) {
 										return;
 									}
-
 									$this->addActionToQueue(self::ACTION_CLAIM_VOTE, $player);
-
 									break;
 								case self::VOTE_STATUS_CLAIMED:
 									$player->sendMessage(TextFormat::colorize($plugin->getConfig()->getNested("messages.vote.alreadyclaimed", "&cYou have already claimed your vote.")));
-
 									break;
 								default:
 									$player->sendMessage(TextFormat::colorize($plugin->getConfig()->getNested("messages.vote.error", "&cUnable to claim your vote, please try again later.")));
-
 									break;
 							}
 						}
-
 						break;
 					case self::ACTION_CLAIM_VOTE:
 						$player = $server->getPlayerExact($result["player"]);
-						if($player !== null && $player->isOnline()){
-							if($result["result"] === "1"){
-								foreach(BetterVotingCache::getCommands() as $command){
+						if ($player !== null && $player->isOnline()) {
+							if ($result["result"] === "1") {
+								foreach (BetterVotingCache::getCommands() as $command) {
 									$server->getCommandMap()->dispatch(new ConsoleCommandSender(), str_replace([
 										"{username}",
 										"{displayname}",
@@ -289,33 +267,29 @@ final class BetterVotingThread extends Thread{
 										$player->getFloorZ()
 									], $command));
 								}
-								foreach($player->getInventory()->addItem(...BetterVotingCache::getItems()) as $overflow){
-									if(!$plugin->getConfig()->get("droprewards", true)){
+								foreach ($player->getInventory()->addItem(...BetterVotingCache::getItems()) as $overflow) {
+									if (!$plugin->getConfig()->get("droprewards", true)) {
 										break;
 									}
-
 									$player->getLevel()->dropItem($player, $overflow);
 								}
 
 								$player->sendMessage(TextFormat::colorize($plugin->getConfig()->getNested("messages.vote.claimed", "&aThank you for voting! You have received your rewards.")));
-							}else{
+							} else {
 								$player->sendMessage(TextFormat::colorize($plugin->getConfig()->getNested("messages.vote.error", "&cUnable to claim your vote, please try again later.")));
 							}
 						}
-
 						break;
 					case self::ACTION_UPDATE_CACHE:
 						BetterVotingCache::setServerInfo($result["info"]);
 						BetterVotingCache::setTopVoters($result["top"]);
 						BetterVotingCache::setUnclaimedVotes($unclaimed = $result["unclaimed"]);
-
-						foreach($unclaimed as $target){
+						foreach ($unclaimed as $target) {
 							$target = $server->getPlayerExact($target);
-							if($target !== null && $target->isOnline()){
+							if ($target !== null && $target->isOnline()) {
 								$this->addActionToQueue(self::ACTION_CLAIM_VOTE, $target);
 							}
 						}
-
 						break;
 				}
 			}
@@ -327,18 +301,17 @@ final class BetterVotingThread extends Thread{
 	 *
 	 * @param string $apiKey
 	 */
-	public function setApiKey(string $apiKey) : void{
+	public function setApiKey(string $apiKey) : void {
 		$this->apiKey = $apiKey;
-
-		if(!$this->running && !$this->isStarted() && $apiKey !== ""){
+		if (!$this->running && !$this->isStarted() && $apiKey !== "") {
 			$this->running = true;
 			$this->start();
-		}elseif($apiKey === ""){
-			if($this->running){
+		} elseif ($apiKey === "") {
+			if ($this->running) {
 				$this->running = false;
 			}
-
 			Server::getInstance()->getLogger()->notice("[BetterVoting] An empty API Key has been provided, the thread will not run until an API Key has been provided in the config.");
 		}
 	}
+
 }
